7c9e49eff4c6b95a68b97788d62a9b66d4cf3090
Aniket <aniket@clayfin.com>
2024-08-18 18:25:49 +0530

    real world

---
+++ springboot-realwold-project/src/main/java/com/advaya/springboot_realwold_project/SpringbootRealwoldProjectApplication.java
@@ -0,0 +1,13 @@
+package com.advaya.springboot_realwold_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRealwoldProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRealwoldProjectApplication.class, args);
+	}
+
+}
---
+++ springboot-realwold-project/src/test/java/com/advaya/springboot_realwold_project/SpringbootRealwoldProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advaya.springboot_realwold_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRealwoldProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
